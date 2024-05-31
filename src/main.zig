8589cd711e108868cae813fccc84d02abf4fc4ee
Youngho Cho <dominoyh5@gmail.com>
2024-05-31 21:27:15 +0900

    update: post

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
-    const uri = try std.Uri.parse("http://localhost:8080/signin");
+    const uri = try std.Uri.parse("http://localhost:8080/signup");
 
     var client = std.http.Client {
         .allocator = allocator,
@@ -11,10 +11,25 @@ pub fn main() !void {
     var server_header_buffer: [4096]u8 = undefined;
     const request_options = std.http.Client.RequestOptions {
         .server_header_buffer = &server_header_buffer,
+        .headers = .{
+            .content_type = std.http.Client.Request.Headers.Value {
+                .override = "application/json",
+            },
+        },
     };
 
     var request = try client.open(std.http.Method.POST, uri, request_options);
+    const payload =
+        \\{
+        \\  "username": "measdf",
+        \\  "password": "measdf"
+        \\}
+    ;
+    request.transfer_encoding = .{ .content_length = payload.len};
+
     try request.send();
+    try request.writeAll(payload);
+    try request.finish();
     try request.wait();
 
     var buffer: [4096]u8 = undefined;
